42845cf348c0d400127a610708c2f8a1990d3887
Misteriya89 <misteriya89@mail.ru>
2023-10-30 10:25:34 +0300

    добавила тестÐовые классы

--- src/test/java/BunMockTest.java
+++ src/test/java/BunMockTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class BunMockTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import praktikum.Bun;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BunMockTest {
+
+    @Mock
+    Bun bunMock;
+
+    @Test
+    public void getNameBunTest() {
+        bunMock.getName();
+        bunMock.getName();
+        Mockito.verify(bunMock, Mockito.times(2)).getName();
+    }
+
+    @Test
+    public void getNameBunStabTest() {
+        Mockito.when(bunMock.getName()).thenReturn("Чизбургер");
+        Assert.assertEquals("Чизбургер", bunMock.getName());
+    }
+
+    @Test
+    public void getPriceBunTest() {
+        bunMock.getPrice();
+        bunMock.getPrice();
+        bunMock.getPrice();
+        Mockito.verify(bunMock, Mockito.times(3)).getPrice();
+    }
+
+    @Test
+    public void getPriceBunStabTest() {
+        Mockito.when(bunMock.getPrice()).thenReturn(3000.0f);
+        Assert.assertEquals(3000.0f, bunMock.getPrice(), 0);
+    }
 }
--- src/test/java/BunParameterizedTest.java
+++ src/test/java/BunParameterizedTest.java
@@ -1,2 +1,45 @@
-package PACKAGE_NAME;public class BunParameterizedTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+@RunWith(Parameterized.class)
+public class BunParameterizedTest {
+
+    private final String name;
+    private final float price;
+
+    public BunParameterizedTest(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunParameters() {
+        return new Object[][]{
+                {"", 50},
+                {"Булочка с соленой карамелью", Float.MIN_VALUE},
+                {"<Бургер Черная Мамба>",  -Float.MAX_VALUE},
+                {String.format("%25c", ' ').replaceAll(" ", "\\" + "Э")
+                        + "то " + String.format("%25c", ' ').replaceAll(" ", "\\" + "о")
+                        + "чень дл" + String.format("%25c", ' ').replaceAll(" ", "\\" + "и")
+                        + "нная стр" + String.format("%25c", ' ').replaceAll(" ", "\\" + "о")
+                        + "ка", Float.MAX_VALUE},
+                {"@$%&*()^%$", 0},
+                {null, -0.01f}
+        };
+    }
+
+    @Test
+    public void getNameBunParameterizedTest() {
+        Bun bun = new Bun(name, price);
+        Assert.assertEquals(name, bun.getName());
+    }
+
+    @Test
+    public void getPriceBunParameterizedTest() {
+        Bun bun = new Bun(name, price);
+        Assert.assertEquals(price, bun.getPrice(), 0);
+    }
 }
